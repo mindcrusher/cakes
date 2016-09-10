039fbe5d1fd6e4901b923ee53d55e54dba40a6e6
Ivan Mazikin <i.mazikin@gmail.com>
2016-09-10 13:34:50 +0300

    different config

--- config/console.php
+++ config/console.php
@@ -20,7 +20,13 @@ $config = [
                 ],
             ],
         ],
-        'db' => $db,
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'pgsql:host=192.168.1.31;dbname=cakeshop',
+            'username' => 'cakeshop',
+            'password' => '96a945',
+            'charset' => 'utf8',
+        ],
     ],
     'params' => $params,
     /*
